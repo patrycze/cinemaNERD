f951078f224390ed9187e2518f9297371c997ab5
Patryk Pazura <patryk.pazura@gmail.com>
2017-12-09 22:57:52 +0100

    initial commit NERD

---
+++ src/app/hall/hall.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hall',
+  templateUrl: './hall.component.html',
+  styleUrls: ['./hall.component.css']
+})
+export class HallComponent {
+  isSelectedArray = [];
+
+  rows = Array.apply(null, Array(10)).map((val, idx) => idx+1);
+  columns = Array.apply(null, Array(5)).map((val, idx) => idx+1);
+
+
+  onPress(column: number, row: number){
+    console.log(column, row);
+    var index = this.isSelectedArray.findIndex(x => x.column == column && 
+                                                                        x.row == row);
+    console.log(index);
+    
+    if(index < 0) {
+      console.log('add');
+      if(index === -1) {
+        this.isSelectedArray.push({column, row});
+      }
+    } else {
+      console.log('remove');
+      this.isSelectedArray.splice(index, 1);
+    }
+    console.log(this.isSelectedArray)
+  }
+
+  isColorSelected(column, row) {
+    return this.isSelectedArray.findIndex(x => x.column == column && 
+      x.row == row) != -1;
+  }
+}
